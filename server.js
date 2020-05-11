1623f992428f6b66f6dce1b072ba92284f3affec
Saget Perez <sagertp@gmail.com>
2020-05-11 00:50:43 -0400

    Cambios menores en el codigo e instalacion de BootStrap, pruebas con imagenes de fondo.

--- server.js
+++ server.js
@@ -4,16 +4,15 @@ const   express = require('express'),
         pug = require('pug')
 
 //app.engine('html', pug.renderFile)
-app.set('views', './app/views')
-app.set('view engine', 'pug')
-app.use('/public', express.static(__dirname + '/public'))
-app.get( '/', (req, res) => res.render('bio.pug') )
-app.get( '/about', (req, res) => res.render('about.pug') )
-app.get( '/telecom', (req, res) => res.render('telecom.pug') )
-app.get( '/devops', (req, res) => res.render('devops.pug') )
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
-
-
-
-
-///
+app
+    .set('views', './app/views')
+    .set('view engine', 'pug')
+app
+    .use('/public', express.static(__dirname + '/public'))
+app
+    .listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app
+    .get( '/', (req, res) => res.render('bio.pug') )
+    .get( '/about', (req, res) => res.render('about.pug') )
+    .get( '/telecom', (req, res) => res.render('telecom.pug') )
+    .get( '/devops', (req, res) => res.render('devops.pug') )
